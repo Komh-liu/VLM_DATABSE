42b1a9fda4d2a6fecdde4747ccacbd770973a245
billgatesbillgates <lz20220619@163.com>
2026-08-12 08:33:24 +0800

    clean

--- research-wiki/update.sh
+++ research-wiki/update.sh
@@ -1,3 +1,3 @@
 # python3 generate-graph-data.py #更新HTML页面
 # cd research-wiki && 
-python3 generate-graph-data.py && git add -A && git commit -m "update wiki" && git push origin HEAD:local-H
+python3 generate-graph-data.py && git add -A && git commit -m "update wiki" && git push origin HEAD:main
